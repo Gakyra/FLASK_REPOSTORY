bd6e97ff18c8d14626d4d44cbdd03f7f7f0a3a8e
Maksim Belosokhov <maks3010dan@gmail.com>
2023-11-05 13:18:51 +0300

    Lesson 8

--- app.py
+++ app.py
@@ -51,6 +51,9 @@ def hobbi_func():
     return "Computer programming, Basketball, walk with friends"
 
 
+
+
+
 @app.route("/create-article", methods=["POST", "GET"])
 def create_article():
     if request.method == "POST":
@@ -73,6 +76,43 @@ def create_article():
     else:
         return render_template("create_article.html")
 
+@app.route("/articles")
+def articles():
+    articles_list = Article.query.order_by(Article.date.desc()).all()
+    return render_template("articles.html", articles=articles_list)
+
+@app.route("/articles/<int:id>")
+def article_detail(id):
+    article = Article.query.get(id)
+    return render_template("article_detail.html", article=article)
+
+@app.route("/articles/<int:id>/del")
+def article_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect("/articles")
+    except Exception as exc:
+        return f"При видаленні виникла помилка: {exc}"
+
+
+@app.route("/articles/<int:id>/update", methods=["POST", "GET"])
+def article_update(id):
+    article = Article.query.get(id)
+
+    if request.method == "POST":
+        article.title = request.form["title"]
+        article.intro = request.form["intro"]
+        article.text= request.form["text"]
+        try:
+            db.session.commit()
+            return redirect("/articles")
+        except Exception as exc:
+            return f"При оновленні запису виникла помилка: {exc}"
+    else:
+        return render_template("post_update.html", article=article)
 
 if __name__ == "__main__":
     app.run(debug=os.getenv("DEBUG"))
