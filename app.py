54f164cab5ee9caa88a823f47ffa49267aa743e6
Maksim Belosokhov <maks3010dan@gmail.com>
2023-11-26 12:43:22 +0300

    Lesson 12 HW  new create user table,edit some changes

--- app.py
+++ app.py
@@ -22,6 +22,15 @@ class Article(db.Model):
     def __repr__(self):
         return f"<Article {self.id}"
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    first_name = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<User {self.id}"
 
 
 
@@ -113,6 +122,56 @@ def article_update(id):
             return f"При оновленні запису виникла помилка: {exc}"
     else:
         return render_template("post_update.html", article=article)
+
+
+
+@app.route("/users")
+def users():
+    users_list = User.query.order_by(User.date.desc()).all()
+    return render_template("user.html", user=users_list)
+
+@app.route("/users/<int:id>")
+def user_detail(id):
+    user = User.query.get(id)
+    return render_template("user_detail.html", user=user)
+
+@app.route("/users/<int:id>/del")
+def user_delete(id):
+    user = User.query.get_or_404(id)
+
+    try:
+        db.session.delete(user)
+        db.session.commit()
+        return redirect("/users")
+    except Exception as exc:
+        return f"При видаленні виникла помилка: {exc}"
+
+
+@app.route("/create-user", methods=["POST", "GET"])
+def create_user():
+    if request.method == "POST":
+        # Отримання даних з форми
+        title = request.form["title"]
+        intro = request.form["intro"]
+        text = request.form["text"]
+
+        # Створення нового об'єкта користувача
+        user = Article(
+            title=title,
+            intro=intro,
+            text=text
+        )
+
+        try:
+            # Збереження користувача в базу даних
+            db.session.add(user)
+            db.session.commit()
+            return redirect("/users")
+        except Exception as exc:
+            return f"При збереженні користувача виникла помилка: {exc}"
+    else:
+        return render_template("create_user.html")
+
 
 if __name__ == "__main__":
     app.run(debug=os.getenv("DEBUG"))
